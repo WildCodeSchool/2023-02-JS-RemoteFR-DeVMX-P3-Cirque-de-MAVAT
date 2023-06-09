7848b7976566d068c900bd323c89708ca92f8ecc
webdevbynight <contact@victor-brito.name>
2023-06-09 10:49:46 +0200

    Created route for works, all found

--- backend/src/router.js
+++ backend/src/router.js
@@ -3,11 +3,14 @@ const express = require("express");
 const router = express.Router();
 
 const itemControllers = require("./controllers/itemControllers");
+const worksControllers = require("./controllers/worksControllers");
 
 router.get("/items", itemControllers.browse);
 router.get("/items/:id", itemControllers.read);
 router.put("/items/:id", itemControllers.edit);
 router.post("/items", itemControllers.add);
 router.delete("/items/:id", itemControllers.destroy);
+
+router.get("/works", worksControllers.browse);
 
 module.exports = router;
