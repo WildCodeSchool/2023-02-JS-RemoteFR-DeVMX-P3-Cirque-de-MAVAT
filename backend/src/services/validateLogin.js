7d0a61a3cd3f37b2bc20890a48387a0caddd83c6
webdevbynight <contact@victor-brito.name>
2023-06-20 08:29:11 +0200

    Added required filter to Joi validation and changed status code to 400 in case of validation error

--- backend/src/services/validateLogin.js
+++ backend/src/services/validateLogin.js
@@ -2,14 +2,14 @@ const joi = require("joi");
 
 const validateLogin = (req, res, next) => {
   const schema = joi.object({
-    email: joi.string().email(),
-    password: joi.string(),
+    email: joi.string().email().presence("required"),
+    password: joi.string().presence("required"),
   });
   const { email, password } = req.body;
   const { value, error } = schema.validate({ email, password });
   if (error) {
     console.error(error.message);
-    res.status(500).send(error.message);
+    res.status(400).send(error.message);
   } else {
     req.body = value;
     next();
