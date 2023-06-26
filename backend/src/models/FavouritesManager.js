5f10abdb36ee756b6449d858a6308b20e0d2b24c
Anne LOUIS <anne.louis51100@gmail.com>
2023-06-26 18:30:03 +0200

    added favourites management

--- backend/src/models/FavouritesManager.js
+++ backend/src/models/FavouritesManager.js
@@ -4,6 +4,29 @@ class FavouritesManager extends AbstractManager {
   constructor() {
     super({ table: "favourites" });
   }
-}
 
+  findAllFavourites(id) {
+    return this.database.query(
+      `SELECT work_id
+    FROM ${this.table}
+    WHERE user_id = ?
+    `,
+      [id]
+    );
+  }
+
+  create(userId, workId) {
+    return this.database.query(
+      `INSERT INTO ${this.table} (user_id, work_id) VALUES (?, ?)`,
+      [userId, workId]
+    );
+  }
+
+  delete(userId, workId) {
+    return this.database.query(
+      `DELETE FROM ${this.table} WHERE user_id = ? AND work_id =?`,
+      [userId, workId]
+    );
+  }
+}
 module.exports = FavouritesManager;
