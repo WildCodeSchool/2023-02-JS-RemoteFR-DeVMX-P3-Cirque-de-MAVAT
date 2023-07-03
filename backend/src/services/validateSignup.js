1d0028153d085aed0dc3fac04a8b2de3744f1cde
webdevbynight <contact@victor-brito.name>
2023-07-03 14:33:48 +0200

    Fixed ignored firstname and lastname fields when filled

--- backend/src/services/validateSignup.js
+++ backend/src/services/validateSignup.js
@@ -4,11 +4,16 @@ const validateSignup = (req, res, next) => {
   const schema = joi.object({
     email: joi.string().email().presence("required"),
     password: joi.string().presence("required"),
-    firstname: joi.string().allow(null),
-    lastname: joi.string().allow(null),
+    firstname: joi.string().max(255, "utf8").allow(null),
+    lastname: joi.string().max(255, "utf8").allow(null),
   });
-  const { email, password } = req.body;
-  const { value, error } = schema.validate({ email, password });
+  const { email, password, firstname, lastname } = req.body;
+  const { value, error } = schema.validate({
+    email,
+    password,
+    firstname,
+    lastname,
+  });
   if (error) {
     console.error(error.message);
     res.status(400).send(error.message);
