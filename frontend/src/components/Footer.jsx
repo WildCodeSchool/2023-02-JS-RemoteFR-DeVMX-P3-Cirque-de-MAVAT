b84b4a686815b79b1c97809a4581e64bbcfbe228
AnthonyLASTERNAS <lespike24@hotmail.fr>
2023-07-05 15:21:07 +0200

    liens-footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,38 +6,82 @@ import logoObjetTemoin from "../assets/logo-objet-temoin.svg";
 function Footer() {
   return (
     <footer className="footer">
-      <div className="footer-left">
-        <div className="logo-afac974">
-          <a href="https://capeline974.re/CAPELINE/CARTOTHEQUE/capeline-accueil.html">
-            <img src={logoAfac974} alt="logo AFAC 974" />
-          </a>
+      <div className="footer-top">
+        <div className="footer-left">
+          <div className="logo-afac974">
+            <a href="https://capeline974.re/CAPELINE/CARTOTHEQUE/capeline-accueil.html">
+              <img src={logoAfac974} alt="logo AFAC 974" />
+            </a>
+          </div>
+          <div className="afac-infos">
+            <p>Association des Familles et Amis de CAPELINE</p>
+            <p>Rue Albert Frejaville, Le Tampon 97430</p>
+            <p>La Réunion</p>
+            <p>Tel : +262 692 85 18 42</p>
+            <div className="footer-links">
+              <p>
+                <Link className="link-color" to="/mention">
+                  Mentions légales
+                </Link>
+              </p>
+              <p>Contact</p>
+            </div>
+          </div>
         </div>
-        <div className="afac-infos">
-          <p>Association des Familles et Amis de CAPELINE</p>
-          <p>Rue Albert Frejaville, Le Tampon 97430</p>
-          <p>La Réunion</p>
-          <p>Tel : +262 692 85 18 42</p>
-          <div className="footer-links">
-            <p>
-              <Link to="/mention">Mentions légales</Link>
-            </p>
-            <p>Crédits</p>
-            <p>Contact</p>
+        <div className="footer-right">
+          <p>Nos Partenaires</p>
+          <div className="logo-ihoi">
+            <a href="https://ihoi.org/app/photopro.sk/ihoi_icono/?">
+              <img src={logoIhoi} alt="logo IHOI" />
+            </a>
+          </div>
+          <div className="logo-objet-temoin">
+            <a href="https://museo.vandanjon.com/index.php/en/">
+              <img src={logoObjetTemoin} alt="logo Objet Témoin" />
+            </a>
           </div>
         </div>
       </div>
-      <div className="footer-right">
-        <p>Nos Partenaires</p>
-        <div className="logo-ihoi">
-          <a href="https://ihoi.org/app/photopro.sk/ihoi_icono/?">
-            <img src={logoIhoi} alt="logo IHOI" />
-          </a>
-        </div>
-        <div className="logo-objet-temoin">
-          <a href="https://museo.vandanjon.com/index.php/en/">
-            <img src={logoObjetTemoin} alt="logo Objet Témoin" />
-          </a>
-        </div>
+      <div className="lien-dev">
+        <p>Copyright @ 2023 AFAC974 - All rights reserved</p>
+        <p>
+          Developed with ♥ by
+          <Link
+            className="linkedin"
+            to="https://www.linkedin.com/in/anne-louis-3326b826b/"
+          >
+            {" "}
+            Anne.L,
+          </Link>
+          <Link
+            className="linkedin"
+            to="https://www.linkedin.com/in/anthony-lasternas-208379269/"
+          >
+            {" "}
+            Anthony.L,
+          </Link>
+          <Link
+            className="linkedin"
+            to="https://www.linkedin.com/in/marine-deveza-959b48279/"
+          >
+            {" "}
+            Marine.D,
+          </Link>
+          <Link
+            className="linkedin"
+            to="https://www.linkedin.com/in/trung-nguyen-4a5229268/"
+          >
+            {" "}
+            Trung.N,
+          </Link>
+          <Link
+            className="linkedin"
+            to="https://www.linkedin.com/in/victor-brito-69040a191/"
+          >
+            {" "}
+            Victor.B
+          </Link>
+        </p>
       </div>
     </footer>
   );
