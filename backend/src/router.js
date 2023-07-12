6304e218fc9cefbf4fe2050596933280ffaa6e7a
webdevbynight <contact@victor-brito.name>
2023-07-12 19:03:30 +0200

    Added route protection and reorganised routes order

--- backend/src/router.js
+++ backend/src/router.js
@@ -19,6 +19,7 @@ const favouritesControllers = require("./controllers/favouritesControllers");
 const validateLogin = require("./services/validateLogin");
 const validateSignup = require("./services/validateSignup");
 const hashPassword = require("./services/hashPassword");
+const verifyToken = require("./services/verifyToken");
 const validateWork = require("./services/validateWork");
 const validateUpdate = require("./services/validateUpdate");
 const removeImage = require("./services/removeImage");
@@ -34,21 +35,15 @@ router.get("/works/:id", worksControllers.read);
 
 router.get("/users", usersControllers.browse);
 router.get("/users/:id", usersControllers.read);
-router.delete("/users/:id", usersControllers.destroy);
 
 router.get("/authors", authorsControllers.browse);
 router.get("/authors/:id", authorsControllers.read);
-router.post("/authors", authorsControllers.add);
-router.put("/authors/:id", authorsControllers.edit);
-router.delete("/authors/:id", authorsControllers.destroy);
 
 router.get("/categories", categoriesControllers.browse);
 
 router.get("/techniques", techniquesControllers.browse);
 
 router.get("/favourites/:id", favouritesControllers.browse);
-router.post("/favourites", favouritesControllers.create);
-router.delete("/favourites/:userId/:workId", favouritesControllers.destroy);
 
 router.get("/images/:id", imagesControllers.read);
 
@@ -58,6 +53,10 @@ router.post(
   loginControllers.login,
   loginControllers.verifyPassword
 );
+
+router.post("/users", validateSignup, hashPassword, usersControllers.create);
+
+router.use(verifyToken);
 
 router.post(
   "/works",
@@ -73,14 +72,22 @@ router.put(
   imagesControllers.edit,
   worksControllers.edit
 );
+
+router.put("/users/:id", validateUpdate, hashPassword, usersControllers.edit);
+router.delete("/users/:id", usersControllers.destroy);
+
+router.post("/authors", authorsControllers.add);
+router.put("/authors/:id", authorsControllers.edit);
+router.delete("/authors/:id", authorsControllers.destroy);
+
+router.post("/favourites", favouritesControllers.create);
+router.delete("/favourites/:userId/:workId", favouritesControllers.destroy);
+
 router.delete(
   "/images/:id/:file",
   removeImage,
   imagesControllers.destroy,
   worksControllers.destroy
 );
-
-router.post("/users", validateSignup, hashPassword, usersControllers.create);
-router.put("/users/:id", validateUpdate, hashPassword, usersControllers.edit);
 
 module.exports = router;
