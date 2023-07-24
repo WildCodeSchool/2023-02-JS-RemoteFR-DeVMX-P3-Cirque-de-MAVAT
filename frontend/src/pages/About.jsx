bc44031975cedb3671824a892c963afae6fb9b46
Trung Nguyen <[trung.nguyenpro@hotmail.com](mailto:trung.nguyenpro@hotmail.com)>
2023-07-24 12:40:55 +0200

    added text, fixed css

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -16,10 +16,20 @@ export default function About() {
         </div>
         <div className="about-info-afac">
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit
-            laboriosam eligendi atque nesciunt aliquam ad beatae reiciendis
-            possimus iste! Ipsa ad eum ipsum dignissimos, similique esse aperiam
-            saepe.
+            Nous sommes une équipe de personnes qui travaillent dans un
+            environnement <b>créatif</b> autour de la <b>pédagogie</b>, et du
+            <b> lien entre générations</b>.
+          </p>
+          <p>
+            Nous faisons des interventions à la demande auprès des
+            collectivités, des établissements scolaires, des entreprises ou des
+            associations :
+          </p>
+          <p>
+            <span>2021</span> : PRE Caisse des Ecoles du Tampon, Lycée Ambroise
+            Vollard <br />
+            <span>2022</span> : Service addictologie, CHU Nord, Lycée Pierre
+            Lagourgue, Amis de l'Université, Fondation Favron ...
           </p>
         </div>
       </div>
@@ -28,19 +38,36 @@ export default function About() {
         <div className="about-objet-temoin">
           <div className="logo-objet-temoin-container">
             <h2>
-              <img
-                src={logoObjetTemoin}
-                alt="Objet Témoin"
-                className="img-logo-objet-temoin"
-              />
+              <a
+                href="https://museo.vandanjon.com/index.php/en/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src={logoObjetTemoin}
+                  alt="Objet Témoin"
+                  className="img-logo-objet-temoin"
+                />
+              </a>
             </h2>
           </div>
           <div className="about-info-objet-temoin">
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-              nihil atque officiis, facilis maxime ab laborum assumenda facere
-              quia quo, repellat voluptas dicta temporibus nemo aperiam libero
-              totam tempora!
+              Objet Témoin est un laboratoire d'étude, de production et de
+              transmission du patrimoine à l'ère du numérique.
+            </p>
+            <p>
+              Depuis deux décennies, nous explorons les opportunités créatives
+              numériques pour la transmission culturelle, la pédagogie et la
+              muséographie.
+            </p>
+            <p>
+              De La Réunion à Bruxelles, avec des associations locales, des
+              musées, des producteurs, des conservateurs de monuments
+              historiques et des écoles publiques, nous fouillons dans le
+              patrimoine physique et immatériel et, humblement, contribuons à
+              fournir des boîtes à outils simples mais précises pour la science,
+              l'éducation et la préservation.
             </p>
           </div>
         </div>
@@ -48,16 +75,25 @@ export default function About() {
           <div className="logo-ihoi-container">
             <h2>
               <div className="background-logo-ihoi">
-                <img src={logoIHOI} alt="IHOI" className="img-logo-ihoi" />
+                <a
+                  href="https://ihoi.org/app/photopro.sk/ihoi_icono/?"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={logoIHOI} alt="IHOI" className="img-logo-ihoi" />
+                </a>
               </div>
             </h2>
           </div>
           <div className="about-info-ihoi">
             <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis,
-              rerum. Nemo illo debitis numquam voluptatem recusandae officia, et
-              nostrum ratione tenetur doloribus animi, unde quibusdam aperiam!
-              laboriosam tenetur.
+              L'IHOI est une institution dédiée à la préservation et à la
+              diffusion du patrimoine iconographique de l'océan Indien.
+            </p>
+            <p>
+              Elle propose une riche collection d'images historiques, de
+              photographies et de cartes anciennes, offrant un voyage visuel à
+              travers l'histoire et la diversité culturelle de la région.
             </p>
           </div>
         </div>
