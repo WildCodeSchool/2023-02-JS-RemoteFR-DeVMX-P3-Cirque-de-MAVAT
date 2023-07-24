e8a054e34f654ba26f2ec891d06eff594c05457a
webdevbynight <contact@victor-brito.name>
2023-07-24 19:18:06 +0200

    Added condition to check whether the user has admin permissions

--- frontend/src/components/AdminAuthorsAdd.jsx
+++ frontend/src/components/AdminAuthorsAdd.jsx
@@ -3,6 +3,8 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 
 import AccountBreadcrumb from "./AccountBreadcrumb";
+import Admin403 from "./Admin403";
+
 import CurrentUserContext from "../contexts/CurrentUser";
 
 export default function AdminAuthorsAdd() {
@@ -67,104 +69,111 @@ export default function AdminAuthorsAdd() {
   }
 
   return (
-    <div>
-      <AccountBreadcrumb breadcrumb={breadcrumb} />
-      <section className="account authors">
-        <h2>Ajouter un auteur</h2>
-        {isAdded ? (
-          <>
-            <p>L’auteur a été ajouté avec succès.</p>
-            <p>
-              <Link to="/account/authors" className="back">
-                Retourner à la liste des auteurs
-              </Link>
-            </p>
-          </>
-        ) : (
-          <form onSubmit={handleSubmit} className="register-form">
-            <label htmlFor="lastname">Nom :</label>
-            <input
-              className="register-input"
-              type="text"
-              id="Lastname"
-              autoComplete="off"
-              onChange={(e) => setLastname(e.target.value)}
-              value={lastname}
-              required
-            />
-            <label htmlFor="firstname">Prénom :</label>
-            <input
-              className="register-input"
-              type="text"
-              id="Firstname"
-              autoComplete="off"
-              onChange={(e) => setFirstname(e.target.value)}
-              value={firstname}
-              required
-            />
-            <label htmlFor="artistname">Nom d'artiste :</label>
-            <input
-              className="register-input"
-              type="text"
-              id="Artistname"
-              autoComplete="off"
-              onChange={(e) => setArtistname(e.target.value)}
-              value={artistname}
-              required
-            />
-            <label htmlFor="birthdate">Date de naissance :</label>
-            <input
-              className="register-input"
-              type="date"
-              id="birthdate"
-              autoComplete="off"
-              onChange={(e) => setBirthdate(e.target.value)}
-              value={birthdate}
-              required
-            />
-            <label htmlFor="deathdate">Date de décès :</label>
-            <input
-              className="register-input"
-              type="date"
-              id="deathdate"
-              autoComplete="off"
-              onChange={(e) => setDeathdate(e.target.value)}
-              value={deathdate}
-              required
-            />
-            <label htmlFor="birthplace">Lieu de naissance :</label>
-            <input
-              className="register-input"
-              type="text"
-              id="birthplace"
-              onChange={(e) => setBirthplace(e.target.value)}
-              value={birthplace}
-              required
-            />
-            <label htmlFor="deathplace">Lieu de décès :</label>
-            <input
-              className="register-input"
-              type="text"
-              id="deathplace"
-              onChange={(e) => setDeathplace(e.target.value)}
-              value={deathplace}
-              required
-            />
-            <label htmlFor="biography">Biographie :</label>
-            <input
-              className="register-input"
-              type="text"
-              id="biography"
-              onChange={(e) => setBiography(e.target.value)}
-              value={biography}
-              required
-            />
-            <button className="register-button" type="submit">
-              Ajouter
-            </button>
-          </form>
-        )}
-      </section>
-    </div>
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    <>
+      {currentUser.isAdmin ? (
+        <>
+          <AccountBreadcrumb breadcrumb={breadcrumb} />
+          <section className="account authors">
+            <h2>Ajouter un auteur</h2>
+            {isAdded ? (
+              <>
+                <p>L’auteur a été ajouté avec succès.</p>
+                <p>
+                  <Link to="/account/authors" className="back">
+                    Retourner à la liste des auteurs
+                  </Link>
+                </p>
+              </>
+            ) : (
+              <form onSubmit={handleSubmit} className="register-form">
+                <label htmlFor="lastname">Nom :</label>
+                <input
+                  className="register-input"
+                  type="text"
+                  id="Lastname"
+                  autoComplete="off"
+                  onChange={(e) => setLastname(e.target.value)}
+                  value={lastname}
+                  required
+                />
+                <label htmlFor="firstname">Prénom :</label>
+                <input
+                  className="register-input"
+                  type="text"
+                  id="Firstname"
+                  autoComplete="off"
+                  onChange={(e) => setFirstname(e.target.value)}
+                  value={firstname}
+                  required
+                />
+                <label htmlFor="artistname">Nom d'artiste :</label>
+                <input
+                  className="register-input"
+                  type="text"
+                  id="Artistname"
+                  autoComplete="off"
+                  onChange={(e) => setArtistname(e.target.value)}
+                  value={artistname}
+                  required
+                />
+                <label htmlFor="birthdate">Date de naissance :</label>
+                <input
+                  className="register-input"
+                  type="date"
+                  id="birthdate"
+                  autoComplete="off"
+                  onChange={(e) => setBirthdate(e.target.value)}
+                  value={birthdate}
+                  required
+                />
+                <label htmlFor="deathdate">Date de décès :</label>
+                <input
+                  className="register-input"
+                  type="date"
+                  id="deathdate"
+                  autoComplete="off"
+                  onChange={(e) => setDeathdate(e.target.value)}
+                  value={deathdate}
+                  required
+                />
+                <label htmlFor="birthplace">Lieu de naissance :</label>
+                <input
+                  className="register-input"
+                  type="text"
+                  id="birthplace"
+                  onChange={(e) => setBirthplace(e.target.value)}
+                  value={birthplace}
+                  required
+                />
+                <label htmlFor="deathplace">Lieu de décès :</label>
+                <input
+                  className="register-input"
+                  type="text"
+                  id="deathplace"
+                  onChange={(e) => setDeathplace(e.target.value)}
+                  value={deathplace}
+                  required
+                />
+                <label htmlFor="biography">Biographie :</label>
+                <input
+                  className="register-input"
+                  type="text"
+                  id="biography"
+                  onChange={(e) => setBiography(e.target.value)}
+                  value={biography}
+                  required
+                />
+                <button className="register-button" type="submit">
+                  Ajouter
+                </button>
+              </form>
+            )}
+          </section>
+        </>
+      ) : (
+        <Admin403 />
+      )}
+    </>
   );
 }
